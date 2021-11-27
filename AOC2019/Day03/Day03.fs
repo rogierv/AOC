1d7e262a5fe8e0a0cebb0be45910f85eda5a2756
Rogier Verkaik <Rogier@getroadmap.com>
2021-11-27 23:31:55 +0100

    + Add Part3

--- AOC2019/Day03/Day03.fs
+++ AOC2019/Day03/Day03.fs
@@ -15,10 +15,10 @@ module Part1 =
         let d = y.[1..] |> int                  // distance
         let c = x |> List.last                  // current
         match direction with
-        | 'R' -> x @ ([0..d] |> List.map (fun y -> (fst c, snd c + y)))
-        | 'L' -> x @ ([0..d] |> List.map (fun y -> (fst c, snd c - y)))
-        | 'U' -> x @ ([0..d] |> List.map (fun y -> (fst c + y, snd c)))
-        | 'D' -> x @ ([0..d] |> List.map (fun y -> (fst c - y, snd c)))
+        | 'R' -> x @ ([1..d] |> List.map (fun y -> (fst c, snd c + y)))
+        | 'L' -> x @ ([1..d] |> List.map (fun y -> (fst c, snd c - y)))
+        | 'U' -> x @ ([1..d] |> List.map (fun y -> (fst c + y, snd c)))
+        | 'D' -> x @ ([1..d] |> List.map (fun y -> (fst c - y, snd c)))
         | _   -> x
     
     let getPath wire = wire |> Array.toList |> List.fold folder [(0, 0)]
@@ -31,11 +31,23 @@ module Part1 =
     let findClosestIntersection wire1 wire2 =
         let discardHome coordinates = coordinates |> List.filter (fun coor -> (fst coor, snd coor) <> (0,0))
         let manhattenDistance (coordinates:(int*int)) = Math.Abs(fst coordinates) + Math.Abs(snd coordinates)
+        
         getIntersections wire1 wire2 
         |> discardHome
-        |> List.map (fun coor -> manhattenDistance coor) |> List.min
+        |> List.map (fun coor -> manhattenDistance coor) 
+        |> List.min
 
     let Solution = findClosestIntersection
 
-//module Part2 =
+module Part2 =
+    let findIndex wirePath position = wirePath |> List.findIndex (fun y -> y = position)
+
+    let Solution wire1 wire2 =
+        let wire1Path = wire1 |> Part1.getPath
+        let wire2Path = wire2 |> Part1.getPath
+        Set.intersect (Set.ofList wire1Path) (Set.ofList wire2Path) 
+            |> Set.toList 
+            |> List.map (fun pos -> findIndex wire1Path pos + findIndex wire2Path pos)
+            |> List.filter (fun x -> x <> 0)
+            |> List.min
     
--- Program.fs
+++ Program.fs
@@ -3,6 +3,6 @@ open Day03
 
 let result = 
     let wires = parse "AOC2019/Day03/Input.txt"
-    Part1.Solution wires.[0] wires.[1]
+    Part2.Solution wires.[0] wires.[1]
 
 printf $"{result}"
